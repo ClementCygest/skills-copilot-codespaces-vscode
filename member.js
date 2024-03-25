757925092bd2f3745d36b3945ac0184eb92bb76a
ClementCygest <159929524+ClementCygest@users.noreply.github.com>
2024-03-25 16:24:19 +0100

    Create member.js

---
+++ member.js
@@ -0,0 +1,46 @@
+function skillsMember() {
+  var skills = [
+    {
+      name: 'HTML',
+      icon: 'fab fa-html5',
+      level: '90'
+    },
+    {
+      name: 'CSS',
+      icon: 'fab fa-css3-alt',
+      level: '90'
+    },
+    {
+      name: 'JavaScript',
+      icon: 'fab fa-js-square',
+      level: '80'
+    },
+    {
+      name: 'React',
+      icon: 'fab fa-react',
+      level: '70'
+    },
+    {
+      name: 'Node.js',
+      icon: 'fab fa-node-js',
+      level: '60'
+    },
+    {
+      name: 'MongoDB',
+      icon: 'fas fa-database',
+      level: '60'
+    },
+    {
+      name: 'Git',
+      icon: 'fab fa-git-alt',
+      level: '80'
+    },
+    {
+      name: 'Photoshop',
+      icon: 'fab fa-adobe',
+      level: '70'
+    }
+  ];
+
+  return skills;
+}
